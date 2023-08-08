d02c842758fdf1783f6ac9ff245bab81495e2277
sruthihsr <sruthihsr@gmail.com>
2023-08-08 20:48:39 +0530

    updated bounding boxes

--- student/objdet_detect.py
+++ student/objdet_detect.py
@@ -234,12 +234,27 @@ def detect_objects(input_bev_maps, model, configs):
     objects = [] 
 
     ## step 1 : check whether there are any detections
+    
+    for obj in detections:
+        id, bev_x, bev_y, z, h, bev_w, bev_l, yaw = obj
 
         ## step 2 : loop over all detections
         
+        x = bev_y / configs.bev_height * (configs.lim_x[1] - configs.lim_x[0])
+        y = bev_x / configs.bev_width * (configs.lim_y[1] - configs.lim_y[0]) - (configs.lim_y[1] - configs.lim_y[0])/2.0 
+        w = bev_w / configs.bev_width * (configs.lim_y[1] - configs.lim_y[0]) 
+        l = bev_l / configs.bev_height * (configs.lim_x[1] - configs.lim_x[0])
+        
+        
             ## step 3 : perform the conversion using the limits for x, y and z set in the configs structure
+            
+        if ((x >= configs.lim_x[0]) and (x <= configs.lim_x[1])
+            and (y >= configs.lim_y[0]) and (y <= configs.lim_y[1])
+            and (z >= configs.lim_z[0]) and (z <= configs.lim_z[1])):
         
             ## step 4 : append the current object to the 'objects' array
+            
+            objects.append([1, x, y, z, h, w, l, yaw])
         
     #######
     ####### ID_S3_EX2 START #######   
